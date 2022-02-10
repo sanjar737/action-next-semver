2aa4bda8cdef438400572d7f91070a3ff38f7021
Sanjar <ylent2014@gmail.com>
2022-02-10 17:24:15 +0500

    only get next-version

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const github = require("@actions/github");
 const { inc } = require("semver");
 
 try {
-  const githubToken = core.getInput("github-token", { required: true });
-  const semver = core.getInput("semver");
-  const octokit = github.getOctokit(githubToken);
+  const version = core.getInput("version");
   const tags = [
     "major",
     "minor",
@@ -15,7 +13,7 @@ try {
     "prepatch",
     "prerelease",
   ];
-  let selectedTag = null;
+  let nextVersion = version;
 
   const labels = github.context.payload.pull_request.labels.map(
     (el) => el.name
@@ -23,29 +21,15 @@ try {
 
   for (const tag of tags) {
     if (labels.includes(tag)) {
-      selectedTag = tag;
-      break;
+      nextVersion = inc(nextVersion, tag);
     }
   }
 
-  if (!selectedTag) {
-    throw new Error("semver label not found");
+  if (nextVersion === version) {
+    throw new Error("semantic version tag label not found");
   }
 
-  const nextSemver = inc(semver, selectedTag);
-
-  if (!nextSemver) {
-    throw new Error("failed to get next semver");
-  }
-
-  octokit.rest.pulls.update({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    pull_number: github.context.payload.pull_request.number,
-    title: `Release v${nextSemver}`,
-  });
-
-  core.setOutput("next-semver", version);
+  core.setOutput("next-version", nextVersion);
 } catch (error) {
   core.setFailed(error.message);
 }
